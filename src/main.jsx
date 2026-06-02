a73fad70947d5ec794ca18da51f4d13c39682eee
NERIMA NERIMA <nerima22si@mahasiswa.pcr.ac.id>
2026-06-03 02:10:00 +0700

    remove auth import from main

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-import { AuthProvider } from "./contexts/AuthContext";
+// import { AuthProvider } from "./contexts/AuthContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
