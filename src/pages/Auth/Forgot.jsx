bb20eece28f2e2bf0b2857e87f01ad7bf3867131
NERIMA NERIMA <nerima22si@mahasiswa.pcr.ac.id>
2026-06-03 13:46:25 +0700

    add forgot password feature

--- src/pages/Auth/Forgot.jsx
+++ src/pages/Auth/Forgot.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Mail, ArrowLeft } from "lucide-react";
-import { supabase } from "../../../lib/supabase";
+import { supabase } from "../../lib/supabase";
 
 export default function ForgotPassword() {
     const [email, setEmail] = useState("");
